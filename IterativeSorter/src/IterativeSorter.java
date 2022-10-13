f161363b93fa763bbbf76b502987f250057317d8
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-10-13 15:00:28 -0600

    progress

--- IterativeSorter/src/IterativeSorter.java
+++ IterativeSorter/src/IterativeSorter.java
@@ -21,20 +21,32 @@ public class IterativeSorter {
 
 	}
 
-	public void doSelectionSort(IntegerArray arrayToSort) {
-
+	public void doSelectionSort(IntegerArray arrayToSort) {	
+		for(int i = 0; i <= arrayToSort.length()-1; i++) {
+			int minIndex = i;
+			int minIndexValue = arrayToSort.read(minIndex);
+			
+			for (int j = i+1; j < arrayToSort.length(); j++) {			
+                if (arrayToSort.read(j) < minIndexValue) {
+                	minIndex = j;
+                }
+                	
+				arrayToSort.write(minIndex, arrayToSort.read(j));
+				arrayToSort.write(j, minIndexValue);
+				
+			}
+		}
 		
 	}
 
 	public void doInsertionSort(IntegerArray arrayToSort) {
-		for(int i = 0; i < arrayToSort.length()-1; i++) {
+
+		for(int i = 0; i <=  arrayToSort.length()-1; i++) {
 			int j = i - 1;
 			int currentValue = arrayToSort.read(j+1);
 
-			if (j >=0){
+			if (j >=0)				
 				
-				
-
 				while(j>=0 && arrayToSort.read(j) > currentValue) {
 					int lastValue = arrayToSort.read(j);
 					
@@ -47,4 +59,4 @@ public class IterativeSorter {
 		}
 	}
 
-}
+
